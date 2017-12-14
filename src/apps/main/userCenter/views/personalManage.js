3d348ff29cb92b8fdc27557cdb42859917fcf334
kim <kim@globalbestsoft.cn>
2017-12-14 17:11:13 +0800

    修改个人中心页面地址选择

--- src/apps/main/userCenter/views/personalManage.js
+++ src/apps/main/userCenter/views/personalManage.js
@@ -9,6 +9,8 @@ const PersonalManageView = Base.ItemView.extend({
   events: {
     'submit .js-uc-personalManage-form': 'updatePersonalInfoHandler',
     'click .js-uc-reset': 'resetPageHandler',
+    'click .js-uc-address-info': 'addressInfoHandler',
+    'click .js-head-icon-info': 'headIconHandler',
   },
 
   getHeadIconXhr() {
@@ -16,7 +18,23 @@ const PersonalManageView = Base.ItemView.extend({
       url: '/acct/userinfo/headIconList.json',
     })
   },
-
+  getProvinceXhr() {
+    return Global.sync.ajax({
+      url: '/info/city/provincelist.json',
+    })
+  },
+  getCityXhr(data) {
+    return Global.sync.ajax({
+      url: '/info/city/citylist.json',
+      data,
+    })
+  },
+  getAreaXhr(data) {
+    return Global.sync.ajax({
+      url: '/info/city/areaList.json',
+      data,
+    })
+  },
   onRender() {
     const self = this
     this.$form = this.$('.js-uc-personalManage-form')
@@ -29,8 +47,11 @@ const PersonalManageView = Base.ItemView.extend({
     this.$phone = this.$('.js-uc-phone')
     this.$addressDetail = this.$('.js-uc-address-detail')
     this.$province = this.$('.js-uc-province')
+    this.$provinceList = this.$('.js-province-list')
     this.$city = this.$('.js-uc-city')
+    this.$cityList = this.$('.js-city-list')
     this.$area = this.$('.js-uc-area')
+    this.$areaList = this.$('.js-area-list')
     this.$headIconList = this.$('.js-uc-head-icon-list')
 
     Global.sync.ajax({
@@ -70,6 +91,14 @@ const PersonalManageView = Base.ItemView.extend({
                 }
               }
             })
+          self.getProvinceXhr()
+            .done((resp) => {
+              if (resp && resp.result === 0) {
+                if (resp.root) {
+                  self.formateProvinceList(resp.root, 1)
+                }
+              }
+            })
         } else {
           Global.ui.notification.show('获取用户个人信息失败')
         }
@@ -86,11 +115,33 @@ const PersonalManageView = Base.ItemView.extend({
     if (data) {
       this.$headIconList.empty()
       _(data).each((item) => {
-        this.$headIconList.append(`<li class="icon-info js-head-icon-info ${item.id === this.iconId ? 'active' : ''}"><img src="${item.url}" class="head-img"></li>`)
+        this.$headIconList.append(`<li class="icon-info js-head-icon-info ${item.id === this.iconId ? 'active' : ''}" data-id="${item.id}"><img src="${item.url}" class="head-img"></li>`)
       })
     }
   },
-
+  formateProvinceList(data, type) {
+    if (data) {
+      let list = ''
+      if (type === 1) {
+        _(data).each((item) => {
+          list += `<li class="list-info js-uc-address-info" data-id="${item.provinceId}" data-type="${type}">${item.province}</li>`
+        })
+        this.$provinceList.html(list)
+      } else if (type === 2) {
+        _(data).each((item) => {
+          list += `<li class="list-info js-uc-address-info" data-id="${item.cityId}" data-type="${type}">${item.city}</li>`
+        })
+        this.$cityList.html(list)
+      } else {
+        list = '<div class="text-red m-bottom-sm p-left-sm">*若找不到对应的区县项请选择“其他”，然后再详细地址处再完善</div>'
+        _(data).each((item) => {
+          list += `<li class="list-info js-uc-address-info" data-id="${item.areaId}" data-type="${type}">${item.area}</li>`
+        })
+        list += `<li class="list-info js-uc-address-info text-red" data-id="99999" data-type="${type}">其他</li>`
+        this.$areaList.html(list)
+      }
+    }
+  },
   updatePersonalInfoHandler() {
     const self = this
     this.$btnConfirm.button('loading')
@@ -106,6 +157,10 @@ const PersonalManageView = Base.ItemView.extend({
         receiverName: this.$receiver.val(),
         receiverPhone: this.$phone.val(),
         receiverDetailAddr: this.$addressDetail.val(),
+        receiverCityId: this.$province.data('id'),
+        receiverProvinceId: this.$city.data('id'),
+        receiverAreaId: this.$area.data('id'),
+        headIconId: this.iconId,
       },
     })
       .always(() => {
@@ -120,6 +175,50 @@ const PersonalManageView = Base.ItemView.extend({
           Global.ui.notification.show('修改个人信息失败')
         }
       })
+  },
+  addressInfoHandler(e) {
+    const self = this
+    const $target = $(e.currentTarget)
+    const id = $target.data('id')
+    const name = $target.html()
+    const type = $target.data('type') // 1表示省 2表示市 3表示区
+    if (type === 1) {
+      this.$province.attr('data-id', id).html(name)
+      this.$city.removeAttr('data-id').html('市')
+      this.$area.removeAttr('data-id').html('区')
+      this.getCityXhr({ province: name })
+        .done((res) => {
+          if (res && res.result === 0) {
+            if (res.root) {
+              self.formateProvinceList(res.root, 2)
+            }
+          }
+        })
+        .fail((res) => {
+          Global.ui.notification.show(res.msg === 'fail' ? '获取城市列表失败' : res.msg)
+        })
+    } else if (type === 2) {
+      this.$city.attr('data-id', id).html(name)
+      this.$area.removeAttr('data-id').html('区')
+      this.getAreaXhr({ cityId: id })
+        .done((res) => {
+          if (res && res.result === 0) {
+            if (res.root) {
+              self.formateProvinceList(res.root, 3)
+            }
+          }
+        })
+        .fail((res) => {
+          Global.ui.notification.show(res.msg === 'fail' ? '获取城市列表失败' : res.msg)
+        })
+    } else if (type === 3) {
+      this.$area.attr('data-id', id).html(name)
+    }
+  },
+  headIconHandler(e) {
+    const $target = $(e.currentTarget)
+    $target.addClass('active').siblings().removeClass('active')
+    this.iconId = $target.data('id')
   },
 })
 
