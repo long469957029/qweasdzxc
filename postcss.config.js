a8207303933ceede019a7c9bddc265540b83bc95
kandamashiro <641047675@qq.com>
2017-12-17 03:14:48 +0800

    fix bug

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,24 @@
 module.exports = {
   // parser: 'sugarss',
   plugins: {
-    'postcss-cssnext': {},
+    'postcss-cssnext': {
+      browsers: [
+        'last 3 versions',
+        'ie >= 10',
+        'ie_mob >= 10',
+        'ff >= 30',
+        'chrome >= 34',
+        'safari >= 6',
+        'opera >= 12.1',
+        'ios >= 6',
+        'android >= 4.4',
+        'bb >= 10',
+        'and_uc 9.9',
+      ],
+    },
     'postcss-unrgba': {
       method: 'clone',
     },
     'postcss-filter-gradient': {},
-    cssnano: {},
   },
 }
