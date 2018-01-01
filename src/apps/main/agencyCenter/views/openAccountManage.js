5bd73a0bcc88bae9611fdd38f00cfd9a2d1ade2a
kim <kim@globalbestsoft.cn>
2018-01-01 13:21:02 +0800

    修改开户管理

--- src/apps/main/agencyCenter/views/openAccountManage.js
+++ src/apps/main/agencyCenter/views/openAccountManage.js
@@ -1,5 +1,3 @@
-
-
 const TabView = require('com/tabView')
 
 const OpenAccountManageManualView = require('agencyCenter/views/openAccountManage-manual')
@@ -18,35 +16,35 @@ const OnlineRechargeView = TabView.extend({
   className: 'ac-openAccount-view',
 
   initialize() {
-	  const acctInfo = Global.memoryCache.get('acctInfo')
+    const acctInfo = Global.memoryCache.get('acctInfo')
     if (!acctInfo.merchant) {
-    	   _(this.options).extend({
-    		     tabs: [
-    		       {
-    		         label: '手动开户',
-    		         name: 'manual',
-    		         id: 'jsAcManualAccount',
-    		         view: OpenAccountManageManualView,
-    		       },
+      _(this.options).extend({
+        tabs: [
           {
-    		         label: '链接开户',
-    		         name: 'auto',
-    		         id: 'jsAcAutoAccount',
-    		         view: OpenAccountManageAutoView,
-    		       },
-    		     ],
-    		   })
+            label: '手动开户',
+            name: 'manual',
+            id: 'jsAcManualAccount',
+            view: OpenAccountManageManualView,
+          },
+          {
+            label: '链接开户',
+            name: 'auto',
+            id: 'jsAcAutoAccount',
+            view: OpenAccountManageAutoView,
+          },
+        ],
+      })
     } else {
-    	   _(this.options).extend({
-    		     tabs: [
-    		       {
-    		         label: '手动开户',
-    		         name: 'manual',
-    		         id: 'jsAcManualAccount',
-    		         view: OpenAccountManageManualView,
-    		       },
-    		     ],
-    		   })
+      _(this.options).extend({
+        tabs: [
+          {
+            label: '手动开户',
+            name: 'manual',
+            id: 'jsAcManualAccount',
+            view: OpenAccountManageManualView,
+          },
+        ],
+      })
     }
 
     this.subSubAcctXhr = this.getSubAcctXhr()
@@ -56,7 +54,7 @@ const OnlineRechargeView = TabView.extend({
     const html = []
     let flag = false
     if (!_(limitList).isEmpty()) {
-      _(limitList).map((limit) => {
+      _(limitList).each((limit) => {
         if (limit.quotaLimit > 0) {
           flag = true
         }
