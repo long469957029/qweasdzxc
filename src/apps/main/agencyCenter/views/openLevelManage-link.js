049f73e1626030e7db743a2c5a0756e27611d545
kim <kim@globalbestsoft.cn>
2018-01-08 17:53:49 +0800

    修改开户管理 链接开户

--- src/apps/main/agencyCenter/views/openLevelManage-link.js
+++ src/apps/main/agencyCenter/views/openLevelManage-link.js
@@ -1,10 +1,11 @@
 require('../../../../base/scripts/jquery.qrcode.min')
 
-const OpenAccountManageLinkView = Base.ItemView.extend({
+const SearchGrid = require('com/searchGrid')
+
+const OpenAccountManageLinkView = SearchGrid.extend({
 
   template: require('agencyCenter/templates/openAccoutManage-link.html'),
   startOnLoading: true,
-  linkBarTpl: _(require('agencyCenter/templates/openAccountManage-auto-linkBar.html')).template(),
   redPackDetailTpl: _(require('agencyCenter/templates/openReadPackDetail.html')).template(),
 
   events: {
@@ -23,13 +24,63 @@ const OpenAccountManageLinkView = Base.ItemView.extend({
     // 'mouseover .js-qr-code' : 'hoverQrcode',
     // 'mouseout .js-qr-code' : 'hoverhideQrcode'
   },
-
-  getSubAcctLinkXhr(data) {
-    return Global.sync.ajax({
-      url: '/acct/subaccount/getsubacctlink.json',
-      data,
+  initialize () {
+    _(this.options).extend({
+      height: 624,
+      title: '链接管理',
+      tableClass: 'table table-bordered table-center ac-auto-container',
+      columns: [
+        {
+          name: '注册链接',
+          width: '44%',
+        },
+        {
+          name: '开户类型',
+          width: '7%',
+          sortable: true,
+          id: 1,
+        },
+        {
+          name: '返点',
+          width: '7%',
+          sortable: true,
+          id: 2,
+        },
+        {
+          name: '访问人数',
+          width: '7%',
+          sortable: true,
+          id: 3,
+        },
+        {
+          name: '注册人数',
+          width: '10%',
+          sortable: true,
+          id: 4,
+        },
+        {
+          name: '备注',
+          width: '20%',
+          id: 5,
+        },
+        {
+          name: '操作',
+          width: '5%',
+        },
+      ],
+      gridOps: {
+        emptyTip: '暂无开户链接',
+      },
+      ajaxOps: {
+        url: '/acct/subaccount/getsubacctlink.json',
+      },
+      reqData: {
+        pageSize: 12,
+      },
+      listProp: 'root.linkList',
     })
   },
+
 
   delSubAcctLinkXhr(data) {
     return Global.sync.ajax({
@@ -39,123 +90,41 @@ const OpenAccountManageLinkView = Base.ItemView.extend({
   },
 
   onRender() {
-    this.$limit = this.$('.js-ac-quota-container')
-    this.$autoContainer = this.$('.js-ac-auto-container')
-    this.$acOpenAccountAutoForm = this.$('.js-ac-openAccountAuto-form')
-    this.$acUserType = this.$('.js-ac-userType')
-    this.$acAutoReBate = this.$('.js-ac-auto-rebate')
-    this.$acBonusRangePrompt = this.$('.js-ac-bonus-range-Prompt')
-    this.$acAutoRemarkInput = this.$('.js-ac-auto-remarkInput')
-    this.$acLinkPage = this.$('.js-ac-link-page')
-    this.getLinkList({ pageIndex: 0 })
+    SearchGrid.prototype.onRender.apply(this, arguments)
   },
-  getLinkList(page) {
-    const self = this
-    if (page) {
-      _(page).extend({ pageSize: 12 })
-      this.getSubAcctLinkXhr(page).always(() => {
-        self.loadingFinish()
-      })
-        .done((res) => {
-          if (res && res.result === 0) {
-            const data = res.root
-            self.$autoContainer.staticGrid({
-              startOnLoading: false,
-              height: 624,
-              tableClass: 'table table-bordered table-center',
-              colModel: [
-                {
-                  label: '注册链接',
-                  name: 'userLinkUrl',
-                  width: '47%',
-                  formatter(val) {
-                    // let html = `<span class="js-ac-span-link ac-span-link m-right-xs" title="${_(`/register.html?linkId=${val}`).toLink()}">${_(`/register.html?linkId=${val}`).toLink()}</span>`
-                    let html = `<div class="text-left p-left-sm"><input type="text" class="js-ac-span-link input-link" value="${_(`/register.html?linkId=${val}`).toLink()}">`
-                    html += '<button type="button" class="js-ac-btn-link-copy btn m-right-xs ac-btn-link-copy">复制</button>'
-                    html += `<button userLinkUrl="${val}" type="button" class="js-ac-btn-qr-code btn ac-btn-qr-code">二维码</button>`
-                    html += `<a userLinkUrl="${val}" class="js-ac-btn-red-detail ac-btn-red-detail"></a>`
-                    return `${html}</div>`
-                  },
-                },
-                {
-                  label: '开户类型',
-                  name: 'userType',
-                  width: '7%',
-                  formatter(val) {
-                    return val === 1 ? '玩家' : '代理'
-                  },
-                },
-                {
-                  label: '返点',
-                  name: 'subAcctRebate',
-                  width: '7%',
-                  formatter(val) {
-                    return `<span class="js-ac-auto-subAcctRebate" data-subacctrebate="${val}">${_(val).formatDiv(10, { fixed: 1 })}</span>`
-                  },
-                },
-                {
-                  label: '访问数',
-                  name: 'viewNum',
-                  width: '7%',
-                  formatter(val) {
-                    return val
-                  },
-                },
-                {
-                  label: '注册数',
-                  name: 'regUserNum',
-                  width: '7%',
-                  formatter(val) {
-                    return val
-                  },
-                },
-                {
-                  label: '备注',
-                  name: 'userLinkDes',
-                  width: '20%',
-                  formatter(val) {
-                    // if(val){
-                    return `<span class="js-ac-span-remark ac-span-remark" title="${val}">${val}</span><span class="js-ac-auto-remark-updateBtn ac-auto-remark-updateBtn"></span>`
-                    // }else {
-                    //   return '<input type="text" class="js-ac-auto-remark ac-auto-remark" data-parsley-maxlength="20" /><span class="js-ac-auto-remark-saveBtn ac-auto-remark-saveBtn"></span>';
-                    // }
-                  },
-                },
-                {
-                  label: '操作',
-                  name: '',
-                  width: '5%',
-                  formatter(val, index, info) {
-                    return `<span data-userlinkid="${info.userLinkId}" class="js-ac-link-del ac-link-del"></span>`
-                  },
-                },
-              ],
-              row: data.linkList,
-            })
+  renderGrid(gridData) {
+    const rowsData = _(gridData.linkList).map(function(fundTrace, index, betList) {
+      return {
+        columnEls: this.formatRowData(fundTrace, index, betList),
+        dataAttr: fundTrace,
+      }
+    }, this)
 
-            self.$autoContainer.find('tbody tr').each((index, item) => {
-              self.copyLinkHandler($(item))
-            })
-            self.$autoContainer.append('<div class="js-qr-code-div-all qr-code-div-all"><div class="js-qr-code-div-all-closeBtn qr-code-div-all-closeBtn"></div></div>')
+    this.grid.refreshRowData(rowsData, gridData.rowCount, {
+      pageIndex: this.filterHelper.get('pageIndex'),
+      initPagination: false,
+    }).hideLoading()
+  },
 
-            self.initPage()
-          }
-        })
+  formatRowData(rowInfo) {
+    const row = []
+    let html = `<div class="text-left p-left-sm"><input type="text" class="js-ac-span-link input-link" value="${_(`/register.html?linkId=${rowInfo.userLinkUrl}`).toLink()}">`
+    html += '<button type="button" class="js-ac-btn-link-copy btn m-right-xs ac-btn-link-copy">复制</button>'
+    html += `<button userLinkUrl="${rowInfo.userLinkUrl}" type="button" class="js-ac-btn-qr-code btn ac-btn-qr-code">二维码</button>`
+    if (rowInfo.linkType === 1) {
+      html += '<a class="js-ac-btn-red-detail ac-btn-red-detail"></a>'
     }
+    row.push(html)
+    row.push(rowInfo.userType === 0 ? '代理' : '玩家')
+    row.push(`<span class="js-ac-auto-subAcctRebate" data-subacctrebate="${rowInfo.subAcctRebate}">${_(rowInfo.subAcctRebate).formatDiv(10, { fixed: 1 })}</span>`)
+    row.push(rowInfo.viewNum)
+    row.push(rowInfo.regUserNum)
+    row.push(`<span class="js-ac-span-remark ac-span-remark" title="${rowInfo.userLinkDes}">${rowInfo.userLinkDes}</span>
+          <span class="js-ac-auto-remark-updateBtn ac-auto-remark-updateBtn"></span>`)
+    row.push(`<span data-userlinkid="${rowInfo.userLinkId}" class="js-ac-link-del ac-link-del"></span>`)
+    return row
   },
-  initPage(count) {
-    if (count) {
-      const self = this
-      this.$acLinkPage.pagination({
-        pageSize: 10,
-        totalSize: count,
-        onPaginationChange: (num) => {
-          self.getLinkList({ pageIndex: num })
-        },
-      })
-      this.pagination = this.$acLinkPage.pagination('instance')
-    }
-  },
+
   // event handlers
 
   editAutoHandler(e) {
@@ -241,16 +210,12 @@ const OpenAccountManageLinkView = Base.ItemView.extend({
   delSubAcctHander (e) {
     const self = this
     const $target = $(e.currentTarget)
-    const $tr = $target.closest('tr')
+    // const $tr = $target.closest('tr')
     const userlinkid = $target.data('userlinkid')
 
     this.delSubAcctLinkXhr({ linkId: userlinkid }).done((res) => {
       if (res && res.result === 0) {
-        self.$autoContainer.staticGrid('delRow', $tr.index())
-
-        if (self.$autoContainer.find('.js-gl-static-tr').length === 0) {
-          self.$autoContainer.staticGrid('renderEmpty')
-        }
+        self._getGridXhr()
       } else {
         Global.ui.notification.show(res.msg)
       }
