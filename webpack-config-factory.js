04ca2ff95beefca0c63e1b903112fce68797415e
kim <kim@globalbestsoft.cn>
2018-01-09 14:17:14 +0800

    修改团队中心  链接管理

--- webpack-config-factory.js
+++ webpack-config-factory.js
@@ -166,7 +166,7 @@ module.exports = function(options) {
           options: {
           }
         },
-        include: options.debug ? [path.join(__dirname, 'src')] : [path.join(__dirname, 'src'), path.join(__dirname, 'node_modules', 'rambda')],
+        include: options.debug ? [path.join(__dirname, 'src')] : [path.join(__dirname, 'src'), path.join(__dirname, 'node_modules')],
         exclude: /jquery|jqmeter|turn.html4/,
       },
     ]
