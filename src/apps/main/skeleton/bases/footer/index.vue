0350f45d6e9ed7225e378dc47315d82b4436c09a
David <david@globalsoftbest.com>
2018-01-19 15:12:24 +0800

    Fix Bug

--- src/apps/main/skeleton/bases/footer/index.vue
+++ src/apps/main/skeleton/bases/footer/index.vue
@@ -1,274 +1,386 @@
 <template>
   <div class="footer">
     <div class="footer-container">
-      <div>
-        <div class="link-list">
-          <li><a href="">关于我们</a></li>
-          <li><a href="">规则与条款</a></li>
-          <li><a href="">常见问题</a></li>
-          <li><a href="">游戏规则</a></li>
-          <li><a href="">下载中心</a></li>
-          <li><a href="">防劫持教程</a></li>
+      <div class="footer-content">
+        <div>
+          <div class="link-list">
+            <!-- 关于我们 和规则和条款是同一个页面的不同内容页   -->
+            <li><a href="promotion.html" target="_blank">关于我们</a></li>
+            <li><a href="promotion.html" target="_blank">规则与条款</a></li>
+            <!-- 常见问题是去帮助中心 -->
+            <li><a href="#/hc?page=newbie">常见问题</a></li>
+            <!-- 游戏规则也是去帮助中心 -->
+            <li><a href="#/hc?page=newbie">游戏规则</a></li>
+            <li><a href="newDownload.html" target="_blank">下载中心</a></li>
+            <li><a href="/index.html#nb/detail/287">防劫持教程</a></li>
+          </div>
+
+          <div class="download">
+            <span class="download-list">
+              <!-- 下载手机app点击任意icon都是进到下载中心页面 -->
+              <span>下载手机APP：</span>
+              <a class="android-icon" href="newDownload.html" target="_blank"></a>
+              <a class="ios-icon" href="newDownload.html" target="_blank"></a>
+            </span>
+          </div>
+          <p>Copyright © 2016-2017 无限娱乐 版权所有</p>
+          <p>无限娱乐郑重提示：彩票有风险，投注需谨慎，未满18周岁的青少年禁止购买</p>
+          <div class="desc-bottom">
+            <span class="cooperator-list">
+              <span>合作夥伴：</span>
+              <transition-group name="carousel-list" class="flex-row">
+                <div v-for="index in cooperatorList" :key="`cooperator-${index}`" :class="`carousel-item cooperator-icon-${index}`"></div>
+              </transition-group>
+            </span>
+          </div>
         </div>
 
-        <div class="download">
-          <span>下载手机APP：</span>
-          <span class="download-list">
-            <div class="android-icon"></div>
-            <div class="ios-icon"></div>
-          </span>
-        </div>
-        <p>Copyright © 2016-2017 无限娱乐 版权所有</p>
-        <p>无限娱乐郑重提示：彩票有风险，投注需谨慎，未满18周岁的青少年禁止购买</p>
-        <div class="desc-bottom">
-          <span>合作夥伴：</span>
-          <span class="cooperator-list">
-            <div class="cooperator-icon-1"></div>
-            <div class="cooperator-icon-2"></div>
-            <div class="cooperator-icon-3"></div>
-            <div class="cooperator-icon-4"></div>
-            <div class="cooperator-icon-5"></div>
-            <div class="cooperator-icon-6"></div>
-          </span>
-        </div>
-      </div>
-
-      <div>
-        <div class="desc-company">
-          <p>
-            作为行业挑战者，无限娱乐始终将品牌信誉作为发展的第一要务，
-            从游戏选择，产品设计，运营服务的创新上从未妥协。实力让情怀
-            落地，厚积薄发才能带给用户最极致的游戏体验。
-          </p>
-        </div>
-        <div class="follow">
-          <span class="follow-list">
-            <span>关注我们：</span>
-            <div class="wechat-icon"></div>
-            <div class="weibo-icon"></div>
-          </span>
-        </div>
-        <div class="desc-bottom">
-          <div class="duty-list">
-            <span class="duty-icon-1"></span>
-            <span class="duty-icon-2"></span>
-            <span class="duty-icon-3"></span>
+        <div>
+          <div class="desc-company">
+            <p>
+              作为行业挑战者，无限娱乐始终将品牌信誉作为发展的第一要务，
+              从游戏选择，产品设计，运营服务的创新上从未妥协。实力让情怀
+              落地，厚积薄发才能带给用户最极致的游戏体验。
+            </p>
+          </div>
+          <div class="follow">
+            <span class="follow-list">
+              <span>关注我们：</span>
+              <div class="wechat-icon"></div>
+              <div class="weibo-icon"></div>
+            </span>
+          </div>
+          <div class="duty">
+            <span class="duty-list">
+              <span>博彩责任：</span>
+              <transition-group name="carousel-list" class="flex-row">
+                <div v-for="index in dutyList" :key="`duty-${index}`" :class="`carousel-item duty-icon-${index}`"></div>
+              </transition-group>
+            </span>
           </div>
         </div>
       </div>
+      <div class="footer-bg"></div>
     </div>
-    <!-- <div class="footer-title"><a href="promotion.html" target="_blank">关于我们</a></div> -->
-    <!-- Copyright © 2016-2017 无限娱乐 版权所有 -->
-    <!--<ul>
-      <li><a href="promotion.html" target="_blank">品牌介绍</a></li>
-      &lt;!&ndash;<li><a href="merchants.html#/products" target="_blank">品牌介绍</a></li>&ndash;&gt;
-      &lt;!&ndash;<li><a href="merchants.html#/join" target="_blank">联系我们</a></li>&ndash;&gt;
-    </ul>-->
-    <!--  1. 关于我们 2. 规则与条款 3. 常见问题 4. 游戏规则 5. 下载中心 6. 防劫持较成 -->
-    <!-- <div class="footer-title"><a href="#/hc?page=newbie">帮助中心</a></div> -->
-    <!-- <div class="footer-title"><a href="#/hc?page=ssc">彩种介绍</a></div> -->
-    <!-- <div class="footer-title"><a href="newDownload.html" target="_blank">客户端下载</a></div> -->
-    <!-- <div class="footer-title"><a href="/index.html#nb/detail/287">防劫持教程</a></div> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'main-footer'
-}
+  name: "main-footer",
+  mounted() {
+    const vm = this
+    setInterval(function() {
+      vm.rotateCooperatorList()
+    }, 6000)
+    setInterval(function() {
+      vm.rotateDutyList()
+    }, 4000)
+  },
+  data() {
+    return {
+      cooperatorList: _.range(1, 7),
+      dutyList: _.range(1, 4),
+      id: this._uid
+    }
+  },
+  methods: {
+    rotateCooperatorList() {
+      const vm = this
+      const firstCoperator = vm.cooperatorList[0]
+
+      vm.cooperatorList.shift()
+      vm.cooperatorList.push(firstCoperator)
+    },
+
+    rotateDutyList() {
+      const vm = this
+      const firstDuty = vm.dutyList[0]
+      setTimeout(function() {
+        vm.dutyList.shift()
+        vm.dutyList.push(firstDuty)
+      }, 500)
+    }
+  }
+};
 </script>
 
 <style lang="scss"  scoped>
-  @import "~base/styles/variable";
-  .footer {
-    position: relative;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    display: none;
-    height: 280px;
-    background: #21232a;
+@import "~base/styles/variable";
+
+.flex-row {
+  display: flex;
+  flex-direction: row;
+}
+
+.footer {
+  position: relative;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  display: none;
+  height: 280px;
+  background: #21232a;
+}
+
+.footer-content {
+  position: relative;
+  z-index: 1;
+  display: flex;
+  justify-content: space-between;
+
+  > div:nth-child(1) {
+    width: 730px;
   }
 
-  .footer-container {
-    width: 1200px;
-    position: relative;
-    display: flex;
-    margin: 0 auto;
-    padding: 20px 0 30px;
-    justify-content: space-between;
+  > div:nth-child(2) {
+    width: 360px;
+  }
+}
 
-    .footer-bg {
-      width: 602px;
-      height: 280px;
-      background: url('./images/footer-bg.png');
-      position: absolute;
-      right: 0;
-    }
+.footer-bg {
+  width: 602px;
+  height: 280px;
+  background: url("./images/footer-bg.png");
+  position: absolute;
+  right: -85px;
+  top: 0;
+}
 
-    span {
-      font-size: 12px;
-      color: #999;
-    }
+.footer-container {
+  width: 1200px;
+  position: relative;
+  margin: 0 auto;
+  padding: 24px 0 30px;
+
+  span {
+    font-size: 12px;
+    color: #999;
+    white-space: nowrap;
   }
 
-  .link-list {
-    display: flex;
-    flex-direction: row;
-    margin-bottom: 20px;
-
-    li {
-      height: 12px;
-      border-right: #4d4f55;
-      padding: 0 20px;
-      list-style: none;
-    }
-    a {
-      color: white;
-      transition: all .3s;
-      text-decoration: none;
-      font-size: 14px;
-
-      &:hover, &:active {
-        color: $main-deep-color;
-        text-decoration: underline;
-      }
-    }
+  p {
+    font-size: 12px;
+    color: #999;
   }
+}
 
-  .desc-bottom {
-    margin-top: 50px;
+.link-list {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 25px;
 
-    span {
-      font-size: 14px;
-    }
-  }
+  li {
+    height: 12px;
+    border-right: #4d4f55;
+    padding: 0 20px;
+    list-style: none;
 
-  .desc-company {
-    margin: 70px 0 18px;
-  }
-
-  .follow-list, .download-list, .follow-list, .duty-list, .cooperator-list {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-
-
-
-  .android-icon {
-    width: 45px;
-    height: 45px;
-    background: url('./images/android-icon.png');
-    transition: all .3s;
-
-    &:hover, &:active {
-      background: url('./images/android-icon-active.png');
+    &:nth-child(1) {
+      padding-left: 0;
+      padding-right: 20px;
     }
   }
 
-  .wechat-icon {
-    width: 28px;
-    height: 28px;
-    background: url('./images/wechat-icon.png');
+  a {
+    color: white;
+    transition: all 0.3s;
+    text-decoration: none;
+    font-size: 14px;
 
-    &:hover, &:active {
-      background: url('./images/wechat-icon-active.png');
+    &:hover,
+    &:active {
+      color: $main-deep-color;
+      text-decoration: underline;
     }
   }
+}
 
-  .weibo-icon {
-    width: 28px;
-    height: 28px;
-    background: url('./images/weibo-icon.png');
+.desc-bottom {
+  margin-top: 50px;
 
-    &:hover, &:active {
-      background: url('./images/weibo-icon-active.png');
+  span {
+    font-size: 14px;
+  }
+}
+
+.desc-company {
+  margin: 40px 0 10px;
+  line-height: 25px;
+}
+
+.download {
+  margin-bottom: 15px;
+
+  .download-list {
+    > a {
+      margin-right: 10px;
+    }
+
+    > span {
+      margin-right: 3px;
     }
   }
+}
 
-  .duty-list {
-
-    .duty-icon-1 {
-      width: 24px;
-      height: 26px;
+.follow {
+  .follow-list {
+    > span {
+      margin-right: 10px;
     }
-
-    .duty-icon-2 {
-      width: 25px;
-      height: 25px;
-    }
-
-    .duty-icon-3 {
-      width: 25px;
-      height: 24px;
-    }
-  }
-
-
-  .cooperator-list {
 
     > div {
-      margin: 0 15px;
-    }
-
-    .cooperator-icon-1 {
-      width: 66px;
-      height: 22px;
-    }
-
-    .cooperator-icon-2 {
-      width: 94px;
-      height: 20px;
-    }
-
-    .cooperator-icon-3 {
-      width: 96px;
-      height: 24px;
-    }
-
-    .cooperator-icon-4 {
-      width: 84px;
-      height: 21px;
-    }
-
-    .cooperator-icon-5 {
-      width: 86px;
-      height: 25px;
-    }
-
-    .cooperator-icon-6 {
-      width: 65px;
-      height: 22px;
+      margin-right: 10px;
     }
   }
+}
 
-  .ios-icon {
-    width: 45px;
-    height: 45px;
-    background: url('./images/ios-icon.png');
-    transition: all .3s;
+.duty {
+  margin-top: 40px;
 
-    &:hover, &:active {
-      background: url('./images/ios-icon-active.png');
+  .duty-list {
+    > span {
+      font-size: 14px;
+      margin-right: 10px;
+    }
+
+    > span > div {
+      margin-right: 30px;
     }
   }
+}
 
-  @for $i from 1 to 6 {
-    .cooperator-icon-#{$i} {
-      background: url('./images/cooperator-icon-#{$i}.png');
-    }
+.download-list,
+.follow-list,
+.duty-list,
+.cooperator-list {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.android-icon {
+  width: 45px;
+  height: 45px;
+  background: url("./images/android-icon.png");
+  transition: all 0.3s;
+
+  &:hover,
+  &:active {
+    background: url("./images/android-icon-active.png");
+  }
+}
+
+.wechat-icon {
+  width: 28px;
+  height: 28px;
+  background: url("./images/wechat-icon.png");
+
+  &:hover,
+  &:active {
+    background: url("./images/wechat-icon-active.png");
+  }
+}
+
+.weibo-icon {
+  width: 28px;
+  height: 28px;
+  background: url("./images/weibo-icon.png");
+
+  &:hover,
+  &:active {
+    background: url("./images/weibo-icon-active.png");
+  }
+}
+
+.duty-list {
+  .duty-icon-1 {
+    width: 24px;
+    height: 26px;
   }
 
-  @for $i from 1 to 3 {
-    .duty-icon-#{$i} {
-      background: url('./images/duty-icon-#{$i}.png');
-    }
+  .duty-icon-2 {
+    width: 25px;
+    height: 25px;
   }
 
-  @media (min-height: 930px) {
-    .footer {
-      display: block;
-    }
+  .duty-icon-3 {
+    width: 25px;
+    height: 24px;
   }
+}
+
+.cooperator-list {
+  > span > div {
+    margin: 0 15px;
+  }
+
+  .cooperator-icon-1 {
+    width: 66px;
+    height: 22px;
+  }
+
+  .cooperator-icon-2 {
+    width: 94px;
+    height: 20px;
+  }
+
+  .cooperator-icon-3 {
+    width: 96px;
+    height: 24px;
+  }
+
+  .cooperator-icon-4 {
+    width: 84px;
+    height: 21px;
+  }
+
+  .cooperator-icon-5 {
+    width: 86px;
+    height: 25px;
+  }
+
+  .cooperator-icon-6 {
+    width: 65px;
+    height: 22px;
+  }
+}
+
+.ios-icon {
+  width: 45px;
+  height: 45px;
+  background: url("./images/ios-icon.png");
+  transition: all 0.3s;
+
+  &:hover,
+  &:active {
+    background: url("./images/ios-icon-active.png");
+  }
+}
+
+.carousel-item {
+  transition: all 1s;
+}
+
+.carousel-list-leave-active {
+  position: absolute;
+}
+
+@for $i from 1 to 7 {
+  .cooperator-icon-#{$i} {
+    background: url("./images/cooperator-icon-#{$i}.png");
+  }
+}
+
+@for $i from 1 to 4 {
+  .duty-icon-#{$i} {
+    background: url("./images/duty-icon-#{$i}.png");
+  }
+}
+
+@media (min-height: 930px) {
+  .footer {
+    display: block;
+  }
+}
 </style>
