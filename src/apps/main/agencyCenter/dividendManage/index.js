b481e0c456e79945966e9d347f1491b16268ccf4
kim <kim@globalbestsoft.cn>
2018-02-08 18:37:09 +0800

    修改分红签约bug

--- src/apps/main/agencyCenter/dividendManage/index.js
+++ src/apps/main/agencyCenter/dividendManage/index.js
@@ -28,7 +28,7 @@ const DividendManageView = Base.ItemView.extend({
     if (acctInfo.dividendStatus === dividendConfig.getByName('APPLYING').id) {
       // 申请中，渲染 签约页面
       this.$el.html(new AgreementView().render().$el)
-    } else if (acctInfo.dividendStatus === dividendConfig.getByName('APPLIED').id || acctInfo.merchant) {
+    } else if (acctInfo.dividendStatus === dividendConfig.getByName('APPLIED').id || acctInfo.dividendStatus === dividendConfig.getByName('REVISE').id || acctInfo.merchant) {
       // 已开通，渲染 tab页
       // this.$el.html(new TabView({ merchant: acctInfo.merchant, triggerTab: this.options.triggerTab }).render().$el)
       this.$el.html(new MyDividView().render().$el)
