48d516acdfa9dce283276fd7599b7e4b3632ca30
kim <kim@globalbestsoft.cn>
2018-02-12 11:44:05 +0800

    修改弹窗顺序

--- src/apps/main/skeleton/bases/dialogManage/index.vue
+++ src/apps/main/skeleton/bases/dialogManage/index.vue
@@ -2,7 +2,7 @@
   <div>
     <red-pack-account v-if="showIndex === 1" :red-pack-type="0" :key="showIndex" @next="showNext"></red-pack-account>
     <red-pack-account v-if="showIndex === 2" :red-pack-type="1" :key="showIndex" @next="showNext"></red-pack-account>
-    <novice-package v-if="showIndex ===3" :red-pack-type="1" :key="showIndex" @next="showNext"></novice-package>
+    <novice-package v-if="showIndex ===3" :key="showIndex" @next="showNext"></novice-package>
 
   </div>
 </template>
@@ -12,7 +12,7 @@
   export default{
     name: 'dialog-manage',
     components: {
-      RedPackAccount,  // 注册开户红包弹窗
+      RedPackAccount,  // 注册开户红包弹窗 and 老用户回归红包
       NovicePackage,  // 新手礼包弹窗
     },
     data(){
