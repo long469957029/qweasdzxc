888d21be14602ee8e0cd7ceb13361a58b11d133f
owen <owen@globalbestsoft.cn>
2018-02-13 21:11:26 +0800

    帮助中心临时提交

--- src/apps/main/helpCenter/index.vue
+++ src/apps/main/helpCenter/index.vue
@@ -1,34 +1,280 @@
 <template>
-    <div>
-       帮助中心
+  <div class="helpCenter-body">
+    <div class="helpCenter-panel">
+      <div class="hc-select-bar inline-block">
+        <div class="hc-select-bar-desc">
+          <div class="hc-desc-text-lg inline-block">H</div>
+          <div class="inline-block">
+            <div class="hc-desc-text">帮助中心</div>
+            <div><span class="hc-desc-text-sm">elp</span><span class="hc-desc-text-sm">center</span></div>
+          </div>
+        </div>
+        <div class="hc-select-bar-item">
+          <span class="item-img ab"></span>
+          <span class="item-name">新手上路</span>
+        </div>
+        <div class="hc-list-item">
+          <ul>
+            <li class="active" :class="{active: currentView === 'DepositProcess'}" @click="toggleTabs(DepositProcess)">存款流程</li>
+            <li :class="{active: currentView === 'DrawingProcess'}" @click="toggleTabs(DrawingProcess)">提款流程</li>
+            <li :class="{active: currentView === 'LotteryProcess'}" @click="toggleTabs(LotteryProcess)">购彩流程</li>
+            <li :class="{active: currentView === 'DwFAQ'}" @click="toggleTabs(DwFAQ)">存提款FAQ</li>
+            <li :class="{active: currentView === 'LotteryFAG'}" @click="toggleTabs(LotteryFAG)">彩票投注FAQ</li>
+            <li :class="{active: currentView === 'DnsCourse'}" @click="toggleTabs(DnsCourse)">防DNS劫持教程</li>
+          </ul>
+        </div>
+        <div class="hc-select-bar-item">
+          <span class="item-img news"></span>
+          <span class="item-name">彩票玩法</span>
+        </div>
+        <div class="hc-list-item">
+          <ul>
+            <li :class="{active: currentView === 'SscLottery'}" @click="toggleTabs(SscLottery)">时时彩玩法</li>
+            <li :class="{active: currentView === 'SyxwLottery'}" @click="toggleTabs(SyxwLottery)">11选5玩法</li>
+            <li :class="{active: currentView === 'DpLottery'}" @click="toggleTabs(DpLottery)">3D/低频彩玩法</li>
+            <li :class="{active: currentView === 'PksLottery'}" @click="toggleTabs(PksLottery)">PK拾玩法</li>
+            <li :class="{active: currentView === 'KsLottery'}" @click="toggleTabs(KsLottery)">快三玩法</li>
+          </ul>
+        </div>
+        <div class="hc-select-bar-item" :class="{active: currentView === 'RulesAndTerms'}"
+             @click="toggleTabs(RulesAndTerms)">
+          <span class="item-img rules"></span>
+          <span class="item-name">双面盘玩法</span>
+        </div>
+        <div class="hc-list-item">
+          <ul>
+            <li :class="{active: currentView === 'SscPlayer'}" @click="toggleTabs(SscPlayer)">时时彩玩法</li>
+            <li :class="{active: currentView === 'Pk10Player'}" @click="toggleTabs(Pk10Player)">PK10玩法</li>
+            <li :class="{active: currentView === 'LhcPlayer'}" @click="toggleTabs(LhcPlayer)">六合彩玩法</li>
+          </ul>
+        </div>
+      </div>
+      <div class="helpCenter-content inline-block" :is="currentView" keep-alive>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-    export default{
-        name: 'index',
+  import DepositProcess from "./depositProcess";//存款流程
+  import DrawingProcess from "./drawingProcess";//提款流程
+  import LotteryProcess from "./lotteryProcess";//购彩流程
+  import DwFAQ from "./dwFAQ";//存提款FAQ
+  import LotteryFAG from "./lotteryFAG";//彩票投注FAQ
+  import DnsCourse from "./dnsCourse";//防DNS劫持教程
+  import SscLottery from "./sscLottery";//时时彩玩法
+  import SyxwLottery from "./syxwLottery";//11选5玩法
+  import DpLottery from "./dpLottery";//3D/低频彩玩法
+  import PksLottery from "./pksLottery";//PK拾玩法
+  import KsLottery from "./ksLottery";//快三玩法
+  import SscPlayer from "./sscPlayer";//时时彩玩法
+  import Pk10Player from "./pk10Player";//PK10玩法
+  import LhcPlayer from "./lhcPlayer";//六合彩玩法
 
-        data () {
-            return {}
-        },
+  export default{
+    name: 'index',
 
-        props: {},
+    data () {
+      return {
+        DepositProcess: 'DepositProcess',
+        DrawingProcess: 'DrawingProcess',
+        LotteryProcess: 'LotteryProcess',
+        DwFAQ: 'DwFAQ',
+        LotteryFAG: 'LotteryFAG',
+        DnsCourse: 'DnsCourse',
+        SscLottery: 'SscLottery',
+        SyxwLottery: 'SyxwLottery',
+        DpLottery: 'DpLottery',
+        PksLottery: 'PksLottery',
+        KsLottery: 'KsLottery',
+        SscPlayer: 'SscPlayer',
+        Pk10Player: 'Pk10Player',
+        LhcPlayer: 'LhcPlayer',
 
-        components: {},
+        currentView: 'DepositProcess',
+      }
+    },
 
-        mounted () {
-            this.$nextTick(() => {
-            })
-        },
+    props: {},
 
-        watch: {},
+    components: {
+      DepositProcess,
+      DrawingProcess,
+      LotteryProcess,
+      DwFAQ,
+      LotteryFAG,
+      DnsCourse,
+      SscLottery,
+      SyxwLottery,
+      DpLottery,
+      PksLottery,
+      KsLottery,
+      SscPlayer,
+      Pk10Player,
+      LhcPlayer,
+    },
 
-        computed: {},
+    mounted () {
+        this.$nextTick(() => {
+        })
+    },
 
-        filters: {},
+    watch: {
+      '$route'(to) {
+        this.currentView = to.query.page
+      }
+    },
 
-        methods: {}
+    computed: {},
+
+    filters: {},
+
+    methods: {
+      toggleTabs (tabText) {
+        this.currentView = tabText;
+      }
     }
+  }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.helpCenter-body {
+  display: block;
+  min-height: 584px;
+  position: relative;
+  background-color: #f9f9f9;
+  .helpCenter-panel {
+    background-color: #ffffff;
+    font-size: 0;
+    width: 1192px;
+    margin: 0 auto;
+    height: 100%;
+    box-shadow: 0 0 5px rgba(0, 0, 0, .4);
+    .hc-select-bar {
+      float: left;
+      width: 200px;
+      min-height: 850px;
+      background-color: #f8f8f8;
+      .hc-select-bar-desc {
+        padding: 45px 40px;
+        border-bottom: 1px solid #e6e6e6;
+        .hc-desc-text-lg {
+          font-size: 45px;
+          color: $im-line-color;
+        }
+        .hc-desc-text {
+          font-size: 20px;
+          color: $def-black-color;
+          margin-bottom: -5px;
+        }
+        .hc-desc-text-sm {
+          color: $im-line-color;
+          font-size: 12px;
+          margin-right: 3px;
+        }
+      }
+      .hc-select-bar-item {
+        cursor: pointer;
+        padding: 17px 45px;
+        /*border-left: 3px solid #f8f8f8;*/
+        border-bottom: 1px solid #e6e6e6;
+        .item-name {
+          font-size: 14px;
+          margin-left: 8px;
+          color: #333333;
+        }
+        .item-img {
+          display: inline-block;
+          width: 18px;
+          float: left;
+          height: 18px;
+          &.ab {
+            background-image: url('./misc/au-gray.png');
+          }
+          &.news {
+            background-image: url('./misc/au-new-gray.png');
+          }
+          &.rules {
+            background-image: url('./misc/au-private-gray.png');
+          }
+          &.private {
+            background-image: url('./misc/au-res-gray.png');
+          }
+          &.res {
+            background-image: url('./misc/au-rule-gray.png');
+          }
+        }
+        &.active {
+          /*background-color: #ffffff;*/
+          /*border-left: 3px solid #14b1bb;*/
+          .item-name {
+            color: #14b1bb;
+          }
+          .item-img {
+            &.ab {
+              background-image: url('./misc/au-green.png');
+            }
+            &.news {
+              background-image: url('./misc/au-new-green.png');
+            }
+            &.rules {
+              background-image: url('./misc/au-private-green.png');
+            }
+            &.private {
+              background-image: url('./misc/au-res-green.png');
+            }
+            &.res {
+              background-image: url('./misc/au-rule-green.png');
+            }
+          }
+        }
+      }
+      .hc-list-item {
+        cursor: pointer;
+        background-color: #ffffff;
+        color: $new-inverse-color;
+        font-size: 12px;
+        ul, li {
+          width: 100%;
+          list-style: none;
+        }
+        li {
+          width: 125px;
+          height: 49px;
+          line-height: 49px;
+          border-bottom: 1px solid $im-line-color;
+          padding-left: 75px;
+          position: relative;
+          &:before{
+            content: '';
+            width: 5px;
+            height: 5px;
+            border-radius: 50%;
+            background-color: $def-gray-color;
+            position: absolute;
+            top: 22px;
+            left: 60px;
+            display: block;
+          }
+          /*&:last-child{
+            border-bottom: none;
+          }*/
+          &.active {
+            color: $main-deep-color;
+            &:before{
+              background-color: $main-deep-color !important;
+            }
+          }
+        }
+      }
+    }
+    .helpCenter-content {
+      background-color: #ffffff;
+      width: 951px;
+      min-height: 850px;
+      padding: 20px;
+      border-left: 1px solid #e6e6e6;
+    }
+  }
+}
+</style>
 
---
+++ src/apps/main/helpCenter/pk10Player.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <div class="rules-text-panel">
+      <div class="res-header">
+        PK10玩法规则
+      </div>
+      <div class="res-content">
+        <div class="res-desc">
+          <div class="res-desc-tit">单码/双面规则说明</div>
+          <div class="res-desc-second-tit">定位</div>
+          指冠军、亚军、季军、第四、第五、第六、第七、第八、第九、第十名出现的顺序与号码为派彩依据
+          如第一个开奖号码为3号，投注冠军为3则视为中奖，其它号码视为不中奖。
+          <div class="res-desc-second-tit">大小</div>
+          开出的号码大于或等于6为大，小于或等于5为小 。
+          <div class="res-desc-second-tit">单双</div>
+          号码为双数叫双，如4、6；号码为单数叫单，如3、5。
+        </div>
+        <div class="res-desc">
+          <div class="res-desc-tit">龙虎</div>
+          <div class="res-desc-second-tit">冠军龙虎</div>
+          * 龙：冠军号码大于第十名号码视为“龙”中奖，如冠军开出07，第十名开出03。
+          * 虎：冠军号码小于第十名号码视为“虎”中奖，如冠军开出03，第十名开出07。
+          <div class="res-desc-second-tit">亚军龙虎</div>
+          * 龙：亚军号码大于第九名号码视为“龙”中奖，如亚军开出07，第九名开出03。
+          * 虎：亚军号码小于第九名号码视为“虎”中奖，如亚军开出03，第九名开出07。
+          <div class="res-desc-second-tit">季军龙虎</div>
+          * 龙：季军号码大于第八名号码视为“龙”中奖，如季军开出07，第八名开出03。
+          * 虎：季军号码小于第八名号码视为“虎”中奖，如季军开出03，第八名开出07。
+          <div class="res-desc-second-tit">第四名龙虎</div>
+          * 龙：第四名号码大于第七名号码视为“龙”中奖，如第四名开出07，第七名开出03。
+          * 虎：第四名号码小于第七名号码视为“虎”中奖，如第四名开出03，第七名开出07。
+          <div class="res-desc-second-tit">第五名龙虎</div>
+          * 龙：第五名号码大于第六名号码视为“龙”中奖，如第五名开出07，第六名开出03。
+          * 虎：第五名号码小于第六名号码视为“虎”中奖，如第五名开出03，第六名开出07。
+        </div>
+        <div class="res-desc">
+          <div class="res-desc-tit">冠亚军和</div>
+          冠军号码与亚军号码的和值区间为3~19，当投注组合符合冠亚军和值，即视为中奖
+        </div>
+        <div class="res-desc">
+          <div class="res-desc-tit">冠亚军和大小</div>
+          当开奖结果冠军号码与亚军号码的和值大于11为大，投注“和大”则视为中奖；小于等于11为小，投注“和小”则视为中奖。
+        </div>
+        <div class="res-desc">
+          <div class="res-desc-tit">冠亚军和单双</div>
+          当开奖结果冠军号码与亚军号码的和值为单数如9、13，投注“和单”则视为中奖；为双数如12、16，投注“和双”则视为中奖。
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+    export default{
+        name: 'pk10Player',
+
+        data () {
+            return {}
+        },
+
+        props: {},
+
+        components: {},
+
+        mounted () {
+            this.$nextTick(() => {
+            })
+        },
+
+        watch: {},
+
+        computed: {},
+
+        filters: {},
+
+        methods: {}
+    }
+</script>
+
+<style lang="scss" scoped>
+  .rules-text-panel{
+    .res-header {
+      padding: 14px 20px;
+      color: $def-black-color;
+      font-weight: 600;
+      border-bottom: 1px solid $im-line-color;
+      font-size: 14px;
+    }
+    .res-content{
+      color: $def-black-color;
+      padding: 10px 20px;
+      font-size: 12px;
+      .res-desc{
+        padding: 10px 0;
+        .res-desc-tit {
+          font-size: 14px;
+          color: $main-deep-color;
+        }
+        .res-desc-second-tit {
+          font-size: 14px;
+        }
+      }
+    }
+  }
+</style>
+
