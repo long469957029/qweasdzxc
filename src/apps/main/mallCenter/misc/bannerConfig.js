cb3d5691120e5e527f4b8f188be410e3c4d1f280
owen <owen@globalbestsoft.cn>
2018-02-21 11:06:12 +0800

    临时提交

---
+++ src/apps/main/mallCenter/misc/bannerConfig.js
@@ -0,0 +1,18 @@
+
+
+const bannerConfig = [
+  {
+    advUrl: null,
+    picUrl: require('./banner-1.png'),
+  },
+  {
+    advUrl: null,
+    picUrl: require('./banner-2.png'),
+  },
+  {
+    advUrl: null,
+    picUrl: require('./banner-1.png'),
+  },
+]
+
+module.exports = bannerConfig
