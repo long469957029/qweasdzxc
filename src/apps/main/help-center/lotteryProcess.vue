220579b03b16ab68bffa22a57726126eadc89908
owen <owen@globalbestsoft.cn>
2018-02-25 16:09:14 +0800

    帮助中心图片缩小60再次修改

--- src/apps/main/help-center/lotteryProcess.vue
+++ src/apps/main/help-center/lotteryProcess.vue
@@ -9,31 +9,31 @@
           1）打开平台页面，鼠标移至导航栏上面的彩票菜单上，即可出现平台所有的彩种选择，请您直接点击自己喜欢的彩种进入到投注界面。
         </div>
         <div class="res-desc">
-          <img src="./misc/lottery-1.png" />
+          <img src="./misc/lottery-1.png" width="60%" />
         </div>
         <div class="res-desc">
           2）在彩种投注界面，在这里可以选择不同的游戏玩法或者点击“趣味”进入趣味玩法。
         </div>
         <div class="res-desc">
-          <img src="./misc/lottery-2.png" />
+          <img src="./misc/lottery-2.png" width="60%" />
         </div>
         <div class="res-desc">
           3）在对应的玩法下面选择号码进行投注，然后选择“元角分厘”模式，输入投注倍数。
         </div>
         <div class="res-desc">
-          <img src="./misc/lottery-3.png" />
+          <img src="./misc/lottery-3.png" width="60%" />
         </div>
         <div class="res-desc">
           4）再点击“添加号码”，最后点“确认投注”，投注成功。或者您也可以选择“快捷投注”直接投注，无须添加至号码栏，简单快捷。
         </div>
         <div class="res-desc">
-          <img src="./misc/lottery-4.png" />
+          <img src="./misc/lottery-4.png" width="60%" />
         </div>
         <div class="res-desc">
           5）快捷投注无须确认，直接即可投注成功；添加至号码栏中时，点击确认投注会需要再确认一次即可投注成功。
         </div>
         <div class="res-desc">
-          <img src="./misc/lottery-5.png" />
+          <img src="./misc/lottery-5.png" width="60%" />
         </div>
       </div>
     </div>
