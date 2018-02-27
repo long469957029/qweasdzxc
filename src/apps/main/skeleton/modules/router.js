a01e2568f7730431f071c05526377536e199bb94
polo <polo@123>
2018-02-27 11:11:53 +0800

    fix bugfix

--- src/apps/main/skeleton/modules/router.js
+++ src/apps/main/skeleton/modules/router.js
@@ -10,45 +10,18 @@ const RouterModule = Base.Module.extend({
     const self = this
 
     $(document).off('click.router').on('click.router', 'a.router', (e) => {
-      // const $target = $(e.currentTarget)
-      // const hasFind = Global.viewPool.getQuick($target.attr('href'))
-
       self._history.push(Backbone.history.getHash())
-
-      // 20170607-MMC页面隐藏了footer部分，根据实际情况判断是否展示
-      // if (document.body.clientHeight >= 930) {
-      //   $('#footer').css('display', '')
-      // }
-
-      // if (!hasFind) {
-      //   Global.appRouter.navigate(_($target.attr('href')).addHrefArgs({
-      //     _t: _.now(),
-      //   }), { trigger: true, replace: false })
-      // }
-
-      // Global.appRouter.navigate($target.attr('href'), { trigger: false, replace: true })
-
-      // if (hasFind) {
-      //   Global.ui.menu.selectMenuFromCurrentHash()
-      // }
-
-      // e.preventDefault()
-
-      // return false
     })
 
     $(document).off('click.sub-return').on('click.sub-return', 'a.sub-return,button.sub-return', (e) => {
-      self.back()
+      window.app.$router.back()
+
+      this.back()
     })
   },
 
   goTo(hash) {
-    // Global.appRouter.navigate(_(hash).addHrefArgs({
-    //   _t: _.now(),
-    // }), { trigger: true, replace: false })
     window.location.hash = `#/${hash}`
-
-    // Global.appRouter.navigate(hash, { trigger: false, replace: true })
   },
 
   back() {
