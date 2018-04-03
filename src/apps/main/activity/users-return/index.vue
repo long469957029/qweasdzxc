fb9eb7d63ecbac83def108e74bc7e9532f844c52
kim <kim@globalbestsoft.cn>
2018-04-03 18:10:12 +0800

    修改老用户回归活动前端页面

--- src/apps/main/activity/users-return/index.vue
+++ src/apps/main/activity/users-return/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="user-main">
+  <div class="user-main" @mousemove="updateXY">
     <div class="header">
       <div class="main clearfix">
         <div class="text"></div>
@@ -7,7 +7,16 @@
         <div class="money"></div>
       </div>
     </div>
-    <div class="container"></div>
+    <div class="container">
+      <div class="title"></div>
+      <div class="content"></div>
+      <div class="rule">
+
+      </div>
+      <div class="bottom-decoration"></div>
+    </div>
+    <div class="bottom-flower"></div>
+    <div class="flower" ref="flower" :style=changeXY></div>
     <div class="footer"></div>
   </div>
 </template>
@@ -24,12 +33,20 @@
 
     data(){
       return{
-
+        mouseX:0,
+        mouseY:0,
       }
     },
-
+    computed:{
+      changeXY: function () {
+        return `transform: translate(-${this.mouseX/10}px, -${this.mouseY/10}px);`
+      }
+    },
     methods:{
-
+      updateXY(event){
+        this.mouseX = event.offsetX;
+        this.mouseY = event.offsetY;
+      }
     },
 
     mounted(){
@@ -56,6 +73,7 @@
     height: 2000px;
     min-width: 1100px;
     background: linear-gradient(to bottom,#ffe5f4,#cff8fd);
+    overflow-x: hidden;
   }
   .header{
     width: 100%;
@@ -87,10 +105,72 @@
         position: absolute;
         right: -78px;
         top: 172px;
+        animation: tada 1s;
       }
     }
   }
-
+  .container{
+    width: 1081px;
+    height: 1337px;
+    background: url("./assets/main-bg.png") no-repeat;
+    position: relative;
+    top: -651px;
+    z-index: 2;
+    margin: 0 auto;
+    &:before{
+      content: '';
+      display: block;
+      width: 84px;
+      height: 389px;
+      background: url("./assets/main-left-top.png") no-repeat;
+      position: absolute;
+      top: 40%;
+      left: -48px;
+    }
+    &:after{
+      content: '';
+      display: block;
+      width: 118px;
+      height: 520px;
+      background: url("./assets/main-left-top.png") no-repeat;
+      position: absolute;
+      top: 15%;
+      right: -50px;
+      transform:scaleX(-1);
+    }
+    .bottom-decoration{
+      width: 222px;
+      height: 259px;
+      background: url("./assets/main-left-bottom.png") no-repeat;
+      position: absolute;
+      bottom: -67px;
+      left: -4px;
+    }
+    .content{
+      width: 942px;
+      height: 664px;
+      background-color: #5a162d;
+      border-radius: 5px;
+      margin: 0 auto;
+    }
+  }
+  .bottom-flower{
+    width: 406px;
+    height: 374px;
+    background: url("./assets/main-flower.png") no-repeat;
+    position: absolute;
+    right: 294px;
+    bottom: 266px;
+    z-index: 0;
+  }
+  .flower{
+    width: 1817px;
+    height: 624px;
+    background: url("./assets/flower-bg.png") no-repeat;
+    position: absolute;
+    top: 30%;
+    right: 0;
+  }
   .footer{
     width: 100%;
     height: 747px;
