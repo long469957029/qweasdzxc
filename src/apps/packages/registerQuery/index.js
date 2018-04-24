c88522e1289edc76fcab0d9089550b79ca1a7d67
bill <bill@globalbestsoft.cn>
2018-04-24 16:39:52 +0800

    registerQuery

--- src/apps/packages/registerQuery/index.js
+++ src/apps/packages/registerQuery/index.js
@@ -3,12 +3,12 @@ import store from '../../store/index'
 
 import SyncModule from 'skeleton/modules/sync'
 import UIModule from 'skeleton/modules/ui'
-// import DialogModule from 'skeleton/modules/ui/dialog'
+import DialogModule from 'skeleton/modules/ui/dialog'
 import NotificationModule from "@/apps/main/skeleton/modules/ui/notification";
 import MediatorModule from "@/apps/main/skeleton/modules/mediator";
 import OAuthModule from "@/apps/main/skeleton/modules/oauth";
 import OauthMediatorModule from "@/apps/main/skeleton/modules/mediator/oauth";
-import NewsMediatorModule from "@/apps/main/skeleton/modules/mediator/news";
+// import NewsMediatorModule from "@/apps/main/skeleton/modules/mediator/news";
 import DataModule from "@/apps/main/skeleton/modules/data";
 
 window.Global = new window.Base.Application()
@@ -21,10 +21,10 @@ window.Global.module('oauth', OAuthModule)
 window.Global.module('sync', SyncModule)
 window.Global.module('data', DataModule)
 window.Global.module('m.oauth', OauthMediatorModule)
-window.Global.module('m.news', NewsMediatorModule)
+// window.Global.module('m.news', NewsMediatorModule)
 window.Global.module('sync', SyncModule)
 
-// window.Global.module('ui.dialog', DialogModule)
+window.Global.module('ui.dialog', DialogModule)
 
   Object.defineProperty(Vue.prototype, '_', { value: _ })
 
