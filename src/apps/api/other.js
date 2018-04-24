1ccb0c0a0c8cb4ec761fe5c2417daf56a8af4daf
bill <bill@globalbestsoft.cn>
2018-04-24 16:24:03 +0800

    注册查询

--- src/apps/api/other.js
+++ src/apps/api/other.js
@@ -51,8 +51,40 @@ const saveDownGamePwd = ({channelId,password}, then, fail) => {
     .then(then)
     .catch(fail)
 }
+/**
+ * 获取特定注册ID下的用户
+ * * @param linkId
+ * @param username
+ * @param mobile
+ * @param qq
+ * @param wechat
+ * @param time
+ * @param pageSize
+ * @param pageIndex
+ * @param mobile
+ * @param qq
+ * @returns {*|Promise<T>}
+ */
+const getSpecialRegisterIdUserListApi = ({linkId,username, mobile, qq,wechat,time,pageSize,pageIndex},then,fail) => {
+  return $http({
+    url: '/acct/reg/linkRegUserList.json',
+    data: {
+      linkId,
+      username,
+      mobile,
+      qq,
+      wechat,
+      time,
+      pageSize,
+      pageIndex
+    }
+  })
+    .then(then)
+    .catch(fail)
+}
 export {
   getServerListApi,
   getDownGameUserInfo,
-  saveDownGamePwd
+  saveDownGamePwd,
+  getSpecialRegisterIdUserListApi
 }
