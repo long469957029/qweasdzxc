2e04ca0e28a9d125b605527a3e376b657e562eb3
kim <kim@globalbestsoft.cn>
2018-04-28 14:51:18 +0800

    添加活动

---
+++ src/apps/main/activity/annual-reward/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+
+  </div>
+</template>
+<script>
+  export default {
+    name:'annual-reward',
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+
+  .main{
+    width: 100%;
+    height: 100%;
+    background: url("./assets/main-bg.png");
+  }
+</style>
